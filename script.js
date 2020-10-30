a1234675d35b52f9eab09b37df1caf330f70f28f
Daniel Vitor Pereira da Silva <64044840+danielvito-pereira@users.noreply.github.com>
2020-10-29 21:26:20 -0300

    js

--- script.js
+++ script.js
@@ -5,21 +5,28 @@ const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
 
+timer = 0;
 // Adiciona zero inicial aos números <= 9 (apenas para estética):
 
 
 // Executa um timer padrão de minuto / segundo / centésimos:
-
+function runTimer() {
+    theTimer.innerHTML = timer;
+    timer++;
+}
 
 // Verifica se texto digitado com o fornecido na página:
 function spellCheck() { 
-    let textEntered = testArea.value;
+    let textEntered = testArea.value;// o texto inserido for igual a area de text
     console.log(textEntered);
 }
 
 // Inicia o cronômetro:
 function start(){
-    let textEnteredLength = testArea.value.length;
+    let textEnteredLength = testArea.value.length;//
+    if (textEnteredLength === 0) {
+        setInterval(runTimer,10);
+    }
     console.log(textEnteredLength);
 }
 
@@ -29,9 +36,9 @@ function reset() {
 }
 
 // Listeners de eventos para entrada de teclado e o botão de recomeçar:
-testArea.addEventListener("keypress", start, false);
-testArea.addEventListener("keyup", spellCheck, false);
-resetButton.addEventListener("click", reset, false);
+testArea.addEventListener("keypress", start, false);//verifica quando o primeiro botão for pressionado
+testArea.addEventListener("keyup", spellCheck, false);//quando vc termina de pressionar a tecla 
+resetButton.addEventListener("click", reset, false);//botão 
 
 
 
